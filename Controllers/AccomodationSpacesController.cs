65c84aa545b1b783bebd82996884c75a3a57ed40
Rikudou En Sof <rikudouensof@outlook.com>
2022-03-26 19:31:11 +0100

    Authorization

--- Controllers/AccomodationSpacesController.cs
+++ Controllers/AccomodationSpacesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +11,8 @@ using StudentPropertyManagement.Models;
 
 namespace StudentPropertyManagement.Controllers
 {
-    public class AccomodationSpacesController : Controller
+  [Authorize(Roles = "Admin")]
+  public class AccomodationSpacesController : Controller
     {
         private readonly ApplicationDbContext _context;
 
--- Controllers/StudentComplaintsController.cs
+++ Controllers/StudentComplaintsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +11,9 @@ using StudentPropertyManagement.Models;
 
 namespace StudentPropertyManagement.Controllers
 {
-    public class StudentComplaintsController : Controller
+
+  [Authorize]
+  public class StudentComplaintsController : Controller
     {
         private readonly ApplicationDbContext _context;
 
